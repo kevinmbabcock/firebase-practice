a9583b9d250c3bfad9e74af615a46ec5b266cdb0
Kevin M Babcock <kevinmbabcock@icloud.com>
2018-11-26 19:55:06 -0800

    config firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyCB8goldCsXuIYyLXybDP7GiaXAtrtn_AY",
+    authDomain: "fir-practice-81c29.firebaseapp.com",
+    databaseURL: "https://fir-practice-81c29.firebaseio.com",
+    projectId: "fir-practice-81c29",
+    storageBucket: "fir-practice-81c29.appspot.com",
+    messagingSenderId: "771989081918"
+  };
+firebase.initializeApp(config);
+export default firebase;
